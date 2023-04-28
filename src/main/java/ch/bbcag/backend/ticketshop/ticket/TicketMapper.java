75c14a45404880c5416b1b9e229c10b67bda1f7d
Julian Kläsi <bklaej@bbcag.ch>
2023-04-28 13:21:49 +0200

    Es läuf alles

--- src/main/java/ch/bbcag/backend/ticketshop/ticket/TicketMapper.java
+++ src/main/java/ch/bbcag/backend/ticketshop/ticket/TicketMapper.java
@@ -1,6 +1,8 @@
 package ch.bbcag.backend.ticketshop.ticket;
 
 
+import ch.bbcag.backend.ticketshop.event.Event;
+
 public class TicketMapper {
     public static TicketDTO toDTO(Ticket ticket) {
 
@@ -21,7 +23,9 @@ public class TicketMapper {
         ticket.setName(dto.getName());
         ticket.setDescription(dto.getDescription());
         ticket.setAmount(dto.getAmount());
-
+        Event event = new Event();
+        event.setId(dto.getEventId());
+        ticket.setEvent(event);
 
         return ticket;
     }
